2e51b1d7eb6c54febc428a37b2488251d0e0cf1b
Malika7188 <asmanodeny@gmail.com>
2025-09-11 00:16:04 +0300

    feat: add member approval and rejection logic for group admins

--- chama-wallet-backend/handlers/admin_handlers.go
+++ chama-wallet-backend/handlers/admin_handlers.go
@@ -15,3 +15,13 @@ import (
 func NominateAdmin(c *fiber.Ctx) error {
 	groupID := c.Params("id")
 	user := c.Locals("user").(models.User)
+
+	var payload struct {
+		NomineeID string `json:"nominee_id"`
+	}
+
+	if err := c.BodyParser(&payload); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid body"})
+	}
+
+	
