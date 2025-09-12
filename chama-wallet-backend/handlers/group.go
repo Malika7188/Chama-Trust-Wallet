037c1668f719a57741fb84f7ade7b5f4578b7391
Malika7188 <asmanodeny@gmail.com>
2025-09-12 10:48:33 +0300

    feat(groups): auto-deploy Soroban contract on testnet if not configured

--- chama-wallet-backend/handlers/group.go
+++ chama-wallet-backend/handlers/group.go
@@ -1,6 +1,26 @@
 package handlers
 
+import (
+	"fmt"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
+
+	"chama-wallet-backend/config"
+	"chama-wallet-backend/database"
+	"chama-wallet-backend/models"
+	"chama-wallet-backend/services"
+	"chama-wallet-backend/utils"
+)
+
 type CreateGroupRequest struct {
 	Name        string `json:"name"`
 	Description string `json:"description"`
 }
+
+func CreateGroup(c *fiber.Ctx) error {
+	var payload struct {
+		Name        string `json:"name"`
+		Description string `json:"description"`
+	}
