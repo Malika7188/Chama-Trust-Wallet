332577d5c4ea4d2f4a43b93afbf474bdd1392e1c
Malika7188 <asmanodeny@gmail.com>
2025-09-14 21:29:48 +0300

    add Member model with ID primary key

--- chama-wallet-backend/models/user.go
+++ chama-wallet-backend/models/user.go
@@ -19,3 +19,10 @@ type LoginRequest struct {
 	Email    string `json:"email" validate:"required,email"`
 	Password string `json:"password" validate:"required,min=6"`
 }
+
+type RegisterRequest struct {
+	Name     string `json:"name" validate:"required,min=2"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=6"`
+}
+
