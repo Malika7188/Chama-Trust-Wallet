51b7f1e8ff0b4456c16a26461afd8e57de61dafd
Malika7188 <asmanodeny@gmail.com>
2025-09-14 21:21:44 +0300

    added authentication with jwt

--- chama-wallet-backend/middleware/auth.go
+++ chama-wallet-backend/middleware/auth.go
@@ -45,4 +45,29 @@ func AuthMiddleware() fiber.Handler {
 			})
 		}
 
-		
+		c.Locals("user", user)
+		c.Locals("userID", claims.UserID)
+
+		return c.Next()
+	}
+}
+
+// OptionalAuthMiddleware validates JWT tokens but doesn't require them
+func OptionalAuthMiddleware() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		authHeader := c.Get("Authorization")
+		if authHeader != "" && strings.HasPrefix(authHeader, "Bearer ") {
+			token := strings.TrimPrefix(authHeader, "Bearer ")
+			if token != "" {
+				claims, err := services.ValidateJWT(token)
+				if err == nil {
+					user, err := services.GetUserByID(claims.UserID)
+					if err == nil {
+						c.Locals("user", user)
+					}
+				}
+			}
+		}
+		return c.Next()
+	}
+}
