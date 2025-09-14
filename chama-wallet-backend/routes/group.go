30e7686c36cf2f7c03344a49b36c0ccb9a943946
Malika7188 <asmanodeny@gmail.com>
2025-09-14 21:58:13 +0300

    add AdminNomination model with ID primary key

--- chama-wallet-backend/routes/group.go
+++ chama-wallet-backend/routes/group.go
@@ -18,4 +18,10 @@ func GroupRoutes(app *fiber.App) {
 	app.Get("/group/:id", middleware.OptionalAuthMiddleware(), handlers.GetGroupDetails)
 	app.Get("/group/:id/balance", middleware.OptionalAuthMiddleware(), handlers.GetGroupBalance)
 
+	// Protected routes (require authentication)
+	app.Post("/group/create", middleware.AuthMiddleware(), handlers.CreateGroup)
+	app.Get("/user/groups", middleware.AuthMiddleware(), handlers.GetUserGroups)
+	app.Post("/group/:id/contribute", middleware.AuthMiddleware(), handlers.ContributeToGroup)
+	app.Post("/group/:id/join", middleware.AuthMiddleware(), handlers.JoinGroup)
+
 	
