f6276e8c8bb6628203493248d2319466bb574314
Malika7188 <asmanodeny@gmail.com>
2025-09-17 01:15:14 +0300

    init: add services package for Stellar account balance checks

---
+++ chama-wallet-backend/services/balance.go
@@ -0,0 +1,13 @@
+package services
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/stellar/go/clients/horizonclient"
+
+	"chama-wallet-backend/config"
+)
+
+func CheckBalance(address string) (string, error) {
+	
