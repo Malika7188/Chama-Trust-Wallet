a542eb96699ab33f9605fd07cb870764f6d420a2
Malika7188 <asmanodeny@gmail.com>
2025-09-25 08:22:12 +0300

    chore: add services package directory for reusable app logic

---
+++ chama-wallet-backend/services/fund.go
@@ -0,0 +1,15 @@
+package services
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"chama-wallet-backend/config"
+)
+
+// FundTestAccount uses the Stellar Friendbot to send test XLM to a new account (testnet only)
+func FundTestAccount(address string) error {
+	if config.Config.IsMainnet {
+		return fmt.Errorf("friendbot funding not available on mainnet - use real XLM deposits")
+	
