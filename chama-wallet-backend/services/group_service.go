57a0238c582a82735f3f3734985f7d84a1a6f032
Malika7188 <asmanodeny@gmail.com>
2025-09-25 08:30:04 +0300

    feat(groups): implement CreateGroup with auto wallet generation and DB save

---
+++ chama-wallet-backend/services/group_service.go
@@ -0,0 +1,24 @@
+package services
+
+import (
+	"errors"
+	"time"
+
+	"github.com/google/uuid"
+
+	"chama-wallet-backend/database"
+	"chama-wallet-backend/models"
+	"chama-wallet-backend/utils"
+)
+
+var groups = make(map[string]models.Group)
+
+func CreateGroup(name, description, creatorID string) (models.Group, error) {
+	wallet, err := utils.GenerateStellarWallet()
+	if err != nil {
+		return models.Group{}, err
+	}
+
+	group := models.Group{
+		ID:          uuid.New().String(),
+		
