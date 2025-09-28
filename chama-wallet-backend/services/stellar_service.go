ceeed89aedf2a181d856e9f0d68c39ae763a873f
Malika7188 <asmanodeny@gmail.com>
2025-09-28 21:40:25 +0300

    feat: add basic SendPayment function to transfer XLM

---
+++ chama-wallet-backend/services/stellar_service.go
@@ -0,0 +1,14 @@
+package services
+
+import (
+	"fmt"
+
+	"github.com/stellar/go/clients/horizonclient"
+	"github.com/stellar/go/keypair"
+	"github.com/stellar/go/txnbuild"
+
+	"chama-wallet-backend/config"
+)
+
+func SendPayment(fromSecret, toAddress, amount string) error {
+	
