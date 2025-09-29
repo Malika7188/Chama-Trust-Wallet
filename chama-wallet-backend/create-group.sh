03b1076c6f0728c1b3fd39f88769b04a3eef37f0
Malika7188 <asmanodeny@gmail.com>
2025-09-29 20:51:50 +0300

    chore: include contract id variable for future deployments

---
+++ chama-wallet-backend/create-group.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Script to create a new Chama group via the API
+# Usage: ./create-group.sh [group_name] [description] [wallet_address]
+
+# Colors for output
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
