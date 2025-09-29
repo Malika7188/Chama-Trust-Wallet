460019e6313bf0f8d502601e4e4683830baf50f4
Malika7188 <asmanodeny@gmail.com>
2025-09-29 21:11:07 +0300

    chore: add debug-group-contribution.sh script

---
+++ chama-wallet-backend/debug-group-contribution.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Debug script for group contribution issues
+# Usage: ./debug-group-contribution.sh
+
+# Colors for output
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
