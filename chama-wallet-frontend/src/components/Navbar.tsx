04dc66e7e014dbd31eda7d24d84f260c56ac0b84
Malika7188 <asmanodeny@gmail.com>
2025-10-04 23:24:12 +0300

    feat: integrate NotificationCenter in Navbar

--- chama-wallet-frontend/src/components/Navbar.tsx
+++ chama-wallet-frontend/src/components/Navbar.tsx
@@ -2,3 +2,5 @@ import { Link, useLocation } from 'react-router-dom'
 import { useAuth } from '../contexts/AuthContext'
 import { Wallet, Users, BarChart3, LogOut, Menu, X } from 'lucide-react'
 import { useState } from 'react'
+import NotificationCenter from './NotificationCenter'
+
