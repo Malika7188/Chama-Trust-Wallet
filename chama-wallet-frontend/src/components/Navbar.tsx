526dfd4d36ffa04b9fd4c337d374603c5b1ff483
Malika7188 <asmanodeny@gmail.com>
2025-10-04 23:28:59 +0300

    style: make logout button more prominent

--- chama-wallet-frontend/src/components/Navbar.tsx
+++ chama-wallet-frontend/src/components/Navbar.tsx
@@ -23,4 +23,6 @@ const Navbar = () => {
   }
   return (
     <nav className="bg-white shadow-sm border-b border-gray-200">
-      
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between h-16">
+        
