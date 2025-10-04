2097e621bbd694c10bdbbd4a4c2f8720c21a9e2d
Malika7188 <asmanodeny@gmail.com>
2025-10-04 23:36:18 +0300

    enhance: animate logout icon rotation on hover

--- chama-wallet-frontend/src/components/Navbar.tsx
+++ chama-wallet-frontend/src/components/Navbar.tsx
@@ -76,4 +76,9 @@ const Navbar = () => {
                 >
                   {isMobileMenuOpen ? <X className="w-5 h-5" /> : <Menu className="w-5 h-5" />}
                 </button>
-           
+              </>
+            ) : (
+              <div className="flex items-center space-x-4">
+                <Link
+                  to="/login"
+               
