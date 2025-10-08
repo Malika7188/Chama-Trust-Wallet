80baf12da7e494276af85b6b061d2246622a8c18
Malika7188 <asmanodeny@gmail.com>
2025-10-08 23:39:27 +0300

    Setup state for notification dropdown toggle

--- chama-wallet-frontend/src/components/NotificationCenter.tsx
+++ chama-wallet-frontend/src/components/NotificationCenter.tsx
@@ -1,3 +1,4 @@
 import React, { useState } from 'react'
 import { Bell, Check, X, Users, DollarSign, UserPlus } from 'lucide-react'
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import api, { notificationApi } from '../services/api'
