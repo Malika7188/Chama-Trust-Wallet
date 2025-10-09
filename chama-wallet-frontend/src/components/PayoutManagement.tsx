f67dea1d7ddc407f7ce6a1bb3f64c1025473c5f9
Malika7188 <asmanodeny@gmail.com>
2025-10-09 23:35:39 +0300

    feat: add React Query setup for fetching payout requests

--- chama-wallet-frontend/src/components/PayoutManagement.tsx
+++ chama-wallet-frontend/src/components/PayoutManagement.tsx
@@ -4,3 +4,9 @@ import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
 import { groupApi, payoutApi } from '../services/api'
 import type { Group, User, PayoutRequest } from '../types'
 import toast from 'react-hot-toast'
+
+interface PayoutManagementProps {
+  group: Group
+  currentUser: User
+}
+
